79d59f5df46b09d4dd77d8093d0ad66dd29f1dfe
CassiaHelenSrt <cassiahelen9213@gmail.com>
2025-11-05 17:06:17 -0300

    estilizando a header

--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
   templateUrl: './header.html',
   styleUrl: './header.scss',
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
 })
 export class Header {}

44c16bf445685ec822ad16f71ebc2ed38adaf0fb
CassiaHelenSrt <cassiahelen9213@gmail.com>
2025-11-06 16:06:50 -0300

    componente de listagem

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { List } from './features/list/list';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: List,
+  },
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Header } from './header/header';
+import { Header } from './shared/header/header';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, Header],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
-export class App {
-
-}
+export class App {}

7a6d521014c38e22c9756e4fbe0d137120cf7670
CassiaHelenSrt <cassiahelen9213@gmail.com>
2025-11-07 16:24:42 -0300

    Criando componente para cadastrar um produto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { List } from './features/list/list';
+import { Create } from './features/create/create';
 
 export const routes: Routes = [
   {
     path: '',
     component: List,
+  },
+  {
+    path: 'create-product',
+    loadComponent: () => import('./features/create/create').then((m) => m.Create),
   },
 ];
--- src/app/features/list/list.ts
+++ src/app/features/list/list.ts
@@ -1,21 +1,24 @@
 import { Component, inject } from '@angular/core';
 import { Products } from '../../shared/services/products';
 import { Product } from '../../shared/interfaces/product.interface';
-import { MatAnchor } from '@angular/material/button';
+import { MatAnchor, MatButton, MatButtonModule } from '@angular/material/button';
 
 import { MatCardModule } from '@angular/material/card';
 import { Card } from './components/card/card';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [MatAnchor, MatCardModule, Card],
+  imports: [MatCardModule, Card, RouterLink, MatAnchor, MatButtonModule],
   templateUrl: './list.html',
   styleUrl: './list.scss',
 })
 export class List {
   products: Product[] = [];
-  productsService = inject(Products);
+  // productsService = inject(Products);
+
+  constructor(public productsService: Products) {}
 
   ngOnInit() {
     this.productsService.getAll().subscribe((products) => (this.products = products));

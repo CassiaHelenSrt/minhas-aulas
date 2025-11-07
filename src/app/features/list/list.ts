63f99f0ad10dd0bdef88d593c2cad4e0e9e5273c
CassiaHelenSrt <cassiahelen9213@gmail.com>
2025-11-07 10:06:19 -0300

    usando o json e criando o arquivo proxy.config.json ainda nao esta funcionando

--- src/app/features/list/list.ts
+++ src/app/features/list/list.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
 
 @Component({
   selector: 'app-list',
+  standalone: true,
   imports: [],
   templateUrl: './list.html',
   styleUrl: './list.scss',
 })
 export class List {
+  products: any[] = [];
+  httpClient = inject(HttpClient);
 
+  ngOnInit() {
+    this.httpClient
+      .get<any[]>('http://localhost:3000/products')
+      .subscribe((products) => (this.products = products));
+  }
 }

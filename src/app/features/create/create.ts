e1e8542968a55da71bac26b1dd20ceb91eed9858
CassiaHelenSrt <cassiahelen9213@gmail.com>
2025-11-11 16:25:43 -0300

    salvar-produto

--- src/app/features/create/create.ts
+++ src/app/features/create/create.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormGroup, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
+import { Products } from '../../shared/services/products';
 
 @Component({
   selector: 'app-create',
@@ -11,11 +12,18 @@ import { MatButtonModule } from '@angular/material/button';
   styleUrl: './create.scss',
 })
 export class Create {
+  ProductsService = inject(Products);
+
   form = new FormGroup({
     title: new FormControl<string>('', { nonNullable: true, validators: Validators.required }),
   });
 
   onSubmit() {
-    this.form.controls.title.value;
+    this.ProductsService.post({
+      title: this.form.controls.title.value,
+    })
+    .subscribe(() => {
+      alert('sucesso!');
+    });
   }
 }

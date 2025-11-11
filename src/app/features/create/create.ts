65f97ef3203459c14edd195558b6f3586e4ba9ae
CassiaHelenSrt <cassiahelen9213@gmail.com>
2025-11-11 16:08:00 -0300

    formulario-estilo

--- src/app/features/create/create.ts
+++ src/app/features/create/create.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-create',
-  imports: [ReactiveFormsModule, MatFormFieldModule],
+  imports: [ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatButtonModule],
   templateUrl: './create.html',
   styleUrl: './create.scss',
 })
@@ -12,4 +14,8 @@ export class Create {
   form = new FormGroup({
     title: new FormControl<string>('', { nonNullable: true, validators: Validators.required }),
   });
+
+  onSubmit() {
+    this.form.controls.title.value;
+  }
 }
